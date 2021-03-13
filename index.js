fc65d4e81da8aee8170b151aa92847c0f1edfa77
iRahulP <rahulpatil.m30@gmail.com>
2021-03-13 18:19:52 +0530

    rendered ejs

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+const path = require('path');
 const express = require('express');
 const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', function (req, res){
-    console.log(req);
-    res.send('<h1>It\'s Running</h1>');
+app.get('/', function (req, res) {
+    // console.log(__dirname);
+    // res.send('<h1>It\'s Running</h1>');
+    return res.render('home');
 });
 
 
 
-app.listen(port, function(err){
-    if(err){
+app.listen(port, function (err) {
+    if (err) {
         console.log("Error in running Server", err);
     }
-    console.log("Express running on : ",port);
+    console.log("Express running on : ", port);
 });
