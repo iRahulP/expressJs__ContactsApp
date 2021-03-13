59d539d7e2153bbf66604298ba4fa80cd2133940
iRahulP <rahulpatil.m30@gmail.com>
2021-03-14 00:45:12 +0530

    added Delete functionality

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
@@ -8,19 +8,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static('assets'));
 
-// middleware 1
-app.use(function (req, res, next) {
-    req.myName = "IRON";
-    console.log("Middleware 1");
-    next();
-});
+// // middleware 1
+// app.use(function (req, res, next) {
+//     req.myName = "IRON";
+//     console.log("Middleware 1");
+//     next();
+// });
 
-//middleware 2
-app.use(function (req, res, next) {
-    console.log("My Name is ", req.myName);
-    console.log("middleware 2");
-    next();
-});
+// //middleware 2
+// app.use(function (req, res, next) {
+//     console.log("My Name is ", req.myName);
+//     console.log("middleware 2");
+//     next();
+// });
 
 var contactList = [
     {
@@ -28,10 +28,14 @@ var contactList = [
         phone: '88888888'
     },
     {
-        name: 'Tony',
+        name: "Tony Stark",
+        phone: "1234567890"
+    },
+    {
+        name: 'Jojo',
         phone: '00000000'
     }
-];
+]
 
 app.get('/', function (req, res) {
     // console.log(__dirname);
@@ -57,13 +61,12 @@ app.post('/create-contact', function (req, res) {
     });
 
     //    contactList.push(req.body);
-    console.log(req.body);
-    console.log(req.body.name);
-    console.log(req.body.phone);
+    // console.log(req.body);
+    // console.log(req.body.name);
+    // console.log(req.body.phone);
 
     return res.redirect('/');
     // return res.redirect('back');
-
 })
 
 
@@ -73,3 +76,17 @@ app.listen(port, function (err) {
     }
     console.log("Express running on : ", port);
 });
+
+app.get('/delete-contact/:phone', function (req, res) {
+    console.log(req.params);
+    let phone = req.params.phone;
+
+    let contactIndex = contactList.findIndex(contact => contact.phone == phone);
+    console.log(contactIndex);
+
+    if (contactIndex != -1) {
+        contactList.splice(contactIndex, 1);
+    }
+    return res.redirect('back');
+});
+
